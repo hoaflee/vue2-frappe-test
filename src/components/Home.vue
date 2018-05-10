95a734d6de863ad8fab5bd4241d61750baf08d71
hoa le <lehoanghoa1008@gmail.com>
2018-05-10 23:04:09 +0900

    add code

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <heatmap-chart/>
+</template>
+
+<script>
+import HeatmapChart from './SingleProject/HeatmapChart/Index'
+
+export default {
+  components: {
+    HeatmapChart
+  }
+}
+</script>
+
+<style scoped>
+</style>
